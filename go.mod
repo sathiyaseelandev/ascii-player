21e923b7f16a01242382e36c4f13119587cfb12b
Sathiya Sivaraj <codewithsathya@gmail.com>
2024-03-13 10:55:02 +0530

    Play ascii video

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ascii-player
 
 go 1.22.0
+
+require gocv.io/x/gocv v0.35.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"gocv.io/x/gocv"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	video, err := gocv.VideoCaptureFile("./badapple.mp4")
+	if err != nil {
+		fmt.Println("Failed to read video")
+		os.Exit(1)
+	}
+	img := gocv.NewMat()
+	window := gocv.NewWindow("Bad Apple")
+
+	x := 480
+	y := 360
+
+	rows := 36
+	cols := 96
+
+	blockHeight := y / rows
+	blockWidth := x / cols
+
+	for {
+		video.Read(&img)
+		asciiMat := make([][]byte, rows)
+		for i := 0; i < rows; i++ {
+			asciiMat[i] = make([]byte, cols)
+		}
+
+		for row := 0; row < rows; row++ {
+			for col := 0; col < cols; col++ {
+				sum := 0
+				for i := blockHeight * row; i < blockHeight * row + blockHeight - 1; i++ {
+					for j := blockWidth * col; j < blockWidth * col + blockWidth - 1; j++ {
+						sum += int(img.GetVecbAt(i, j)[0])
+					}
+				}
+				avg := sum / (blockHeight * blockWidth)
+				if avg > 127 {
+					asciiMat[row][col] = byte(':')
+				} else {
+					asciiMat[row][col] = byte(' ')
+				}
+			}
+		}
+
+		window.IMShow(img)
+		var str strings.Builder
+		for i := 0; i < rows; i++ {
+			str.WriteString(string(asciiMat[i]))
+			str.WriteString("\n")
+		}
+		fmt.Println(str.String())
+		// window.WaitKey(1)
+	}
 }
